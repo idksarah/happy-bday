b4da1f2fe828ec176aba963838b659268b6dfb1e
idksarah <sarah.p.ngai@gmail.com>
2025-04-25 23:11:08 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	tea "github.com/charmbracelet/bubbletea")
+
+func main() {
+	err := tea.NewProgram(&Model{}, tea.WithAltScreen()).Start()  
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+type Model struct {}
+
+func (m *Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m *Model) View() string {
+	return "happy bday justin!!"
+}
