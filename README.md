2b8ff459b1b4ab5fcfcb928197a5cf056328959b
idksarah <sarah.p.ngai@gmail.com>
2025-04-27 22:16:59 -0700

    animations

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-# bday present fo my bro
+# bday present fo justiin
 
-built using termination (https://github.com/ansoni/termination?tab=readme-ov-file)
+Built using termination (https://github.com/ansoni/termination?tab=readme-ov-file)
 
 # Usage
 - Clone this repo
 - Install go
 - Open this folder
-- Run "go mod init bday && go mod tidy"
+- Run ```go mod init bday && go mod tidy```
 - Go to /bday
-- Run "go build && ./bday"
+- Run ```go build && ./bday```
 
+# Possible Issues
+- Termination uses the outdated version of rtreego. If you install v1.2.0 and have errors running in the termination.go file, downgrade to v1.1.0
+
+# Demo
+![Demo][demo.gif]
--- bday/bday.go
+++ bday/bday.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/ansoni/termination"
@@ -9,46 +8,198 @@ import (
 )
 
 var letter = termination.Shape{
-	"default": []string{
-		` ______________________
+	"default": []string{`
+ ______________________
+|  \__            __/  |
+|     \__      __/     |
+|        \____/        | <-- envelope btw :<
+|                      |
+|______________________|
+   press the up arrow!`,
+		`
+		
+ ______________________
+|  \__            __/  |
+|     \__      __/     |
+|        \____/        | <-- envelope btw :<
+|                      |
+|______________________|
+   press the up arrow!`,
+	},
+}
+
+var letter1 = termination.Shape{
+	"default": []string{`
+ ______________________
 |  \__            __/  |
 |     \__      __/     |
 |        \____/        |
 |                      |
-|______________________|`,
-		`  ______________________
- |  \__            __/  |
- |     \__      __/     |
- |        \____/        |
- |                      |
- |______________________|`,
+|______________________|
+ (type and press enter)`,
+		`
+		
+ ______________________
+|  \__            __/  |
+|     \__      __/     |
+|        \____/        |
+|                      |
+|______________________|
+ (type and press enter)`,
 	},
 }
 
+var letter2 = termination.Shape{
+	"default": []string{`
+ ______________________
+|  \__            __/  |
+|     \__      __/     |
+|        \____/        |
+|                      |
+|______________________|
+i'm happy you're here. i
+ hope things are going 
+         well`,
+		`
+		
+ ______________________
+|  \__            __/  |
+|     \__      __/     |
+|        \____/        |
+|                      |
+|______________________|
+i'm happy you're here. i
+ hope things are going 
+         well`,
+	},
+}
 var dots = termination.Shape{
 	"default": []string{
 		". . .",
-		" . . .",
+		`
+. . .`,
 	},
 }
 
 var gotYouSomething = termination.Shape{
 	"default": []string{
-		"hey! i got you something",
-		" hey! i got you something",
+		`hey! i got you something
+           :0`,
+		`
+hey! i got you something
+           :0`,
 	},
 }
 
 var gift = termination.Shape{
-	"default": []string{},
+	"default": []string{`
+		    _______________
+  /----\\ /----\\ _/|
+ _|-----\/-----||/| |
+/_____/__/_____/| | |
+|    |   |    | | | |
+|    |   |    | | | |
+|    |   |    | | |_/
+|    |   |    | |_/
+|____|___|____|_/  
+     wow!!`, `
+
+		    _______________
+  /----\\ /----\\ _/|
+ _|-----\/-----||/| |
+/_____/__/_____/| | |
+|    |   |    | | | |
+|    |   |    | | | |
+|    |   |    | | |_/
+|    |   |    | |_/
+|____|___|____|_/
+     wow!!`,
+	},
 }
 
-var yesOrYes = termination.Shape{
-	"default": []string{},
+var yes1 = termination.Shape{
+	"default": []string{
+		`        open?
+[X] yes    [] yes!!`,
+		`
+        open?
+[X] yes    [] yes!!`,
+	},
+}
+
+var yes2 = termination.Shape{
+	"default": []string{
+		`        open?
+[] yes    [X] yes!!`,
+		`
+        open?
+[] yes    [X] yes!!`},
 }
 
 var cake = termination.Shape{
-	"default": []string{},
+	"default": []string{`
+    *	      /\	         /\     - 
+    	    ___||____/\____||___     |
+        /   --    ||    --    \
+    \	  |---------------------| 
+        |---///---///---///---|
+      *	|---------------------|   *
+        |---///---///---///---|
+    -	  |_____________________| \
+      /      *    -      |
+
+         happy (late) birthday 
+         justin! i hope you're 
+        having fun in san diego.
+               i miss u!
+  `, `
+    *	      /\	         /\     \ 
+    	    ___||____/\____||___     /
+        /   --    ||    --    \
+    |	  |---------------------| 
+        |---///---///---///---|
+      *	|---------------------|   *
+        |---///---///---///---|
+    \	  |_____________________| |
+      -      *    \      /
+	  
+         happy (late) birthday 
+         justin! i hope you're 
+        having fun in san diego.
+               i miss u!
+  `,
+		`
+      *	   /\ 	        /\     | 
+    		   ___||____/\____||___     -
+    	   /   --    ||    --    \
+     /  |---------------------| 
+    	   |---///---///---///---|
+    	 *	|---------------------|   *
+    	   |---///---///---///---|
+     |	 |_____________________| /
+    	 \      *    |      -
+	
+    	   happy (late) birthday 
+    	   justin! i hope you're 
+    	  having fun in san diego.
+    		        	i miss u!
+`, `
+       *     /\	         /\     / 
+        		___||____/\____||___     \
+        	/   --    ||    --    \
+    -	   |---------------------| 
+        	|---///---///---///---|
+      *	 |---------------------|   *
+        	|---///---///---///---|
+    /	   |_____________________| -
+      |      *    /      \
+  
+    	   happy (late) birthday 
+    	   justin! i hope you're 
+    	  having fun in san diego.
+    		        i miss u!
+`,
+	},
 }
 
 type KirbyData struct {
@@ -60,32 +211,27 @@ func kirbyMovement(t *termination.Termination, e *termination.Entity, position t
 	return position
 }
 
-func printAt(input interface{}) {
-	fmt.Print("\r\033[K")
-	str := fmt.Sprintf("%v", input)
-	termbox.SetCursor(0, 0)
-	print(str)
+func printAt(input string, x int, y int) {
+	termbox.Flush()
+	print(input)
 }
 
 func main() {
-	// Initialize the termination object and the animation entity
 	term := termination.New()
 	term.FramesPerSecond = 2
-	animation := term.NewEntity(termination.Position{40, 5, 0})
+	animation := term.NewEntity(termination.Position{50, 5, 0})
 	animation.Shape = letter
-	animation.Data = KirbyData{GotoX: 40, GotoY: 5}
+	animation.Data = KirbyData{GotoX: 50, GotoY: 5}
 
-	// Start the animation in the background (only once)
 	go term.Animate()
-	printAt("hi!!")
 
-	// Setup termbox input mode
 	termbox.SetInputMode(termbox.InputEsc | termbox.InputMouse)
 
 	var stage int = 0
-	var stageMax int = 5
+	var stageMax int = 8
+	userInput := "______!"
+	// var name string
 
-	// Main input loop
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
@@ -97,12 +243,35 @@ func main() {
 				stage = min(stageMax, stage+1)
 				term, animation = updateAnimation(stage, term, animation, 2)
 			}
-			// if ev.Key == termbox.KeyArrowDown {
-			// 	stage = max(0, stage-1)
-			// 	term, animation = updateAnimation(stage, term, animation, 1)
-			// }
-			// a way to go back would be a good idea but icl idc
+			if stage == 1 {
+				if ev.Key == termbox.KeyEnter {
+					termbox.Flush()
+					// printAt("", 32, 0)
+					// name = userInput
+					termbox.SetCursor(0, 0)
+					stage = min(stageMax, stage+1)
+					term, animation = updateAnimation(stage, term, animation, 2)
+					// print(name)
+				} else {
+					if ev.Key == termbox.KeyBackspace || ev.Key == termbox.KeyBackspace2 {
+						if len(userInput) > 0 {
+							userInput = userInput[:len(userInput)-1]
+						}
+					} else {
+						if userInput == "______!" {
+							userInput = ""
+						}
+						userInput += string(ev.Ch)
+					}
+					printAt("hi "+userInput+"!", 32+len(userInput), 0)
+				}
+			}
 		}
+		// if ev.Key == termbox.KeyArrowDown {
+		// 	stage = max(0, stage-1)
+		// 	term, animation = updateAnimation(stage, term, animation, 1)
+		// }
+		// a way to go back would be a good idea but icl idc
 	}
 }
 
@@ -114,12 +283,25 @@ func updateAnimation(stage int, term *termination.Termination, animation *termin
 	case 0:
 		animation.Shape = letter
 	case 1:
+		animation.Shape = letter1
+		termbox.SetCursor(76, 12)
+	case 2:
+		animation.Shape = letter2
+	case 3:
 		animation.Shape = dots
+	case 4:
+		animation.Shape = gotYouSomething
+	case 5:
+		animation.Shape = gift
+	case 6:
+		animation.Shape = yes1
+	case 7:
+		animation.Shape = yes2
+	case 8:
+		animation.Shape = cake
 	}
 
 	term.FramesPerSecond = framesPerSec
 
 	return term, animation
 }
-
-// on my life the last commit was 99% me written i just chatgpted this last func and it worked worse than when i wrote it </33 ON MY LIFE!!!
